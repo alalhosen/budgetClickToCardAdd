38f1aa26707c0c400379276c98f2a602d880d191
Alal Hosen <alalhosen1992620120@gmail.com>
2025-07-12 22:31:42 +0600

    set function

--- app.js
+++ app.js
@@ -6,3 +6,12 @@ const convertCartCount = parseInt(cart);
 
 const leftCount = document.getElementById("cart").innerText;
 const convertLeftCount = parseInt(cart); 
+
+
+function getConvertedValue(id) {
+ const price = document.getElementById(id).innerText;
+ const convertPrice = parseInt(price);
+ console.log(convertPrice);
+}
+
+getConvertedValue("budget")
