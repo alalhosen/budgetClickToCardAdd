a15533d7943d42d202c1e041a5ccaa8ca46a961e
Alal Hosen <alalhosen1992620120@gmail.com>
2025-07-15 08:43:32 +0600

    event target set

--- app.js
+++ app.js
@@ -16,7 +16,11 @@ const allBtn = document.getElementsByClassName("add-btn");
 
 for (const btn of allBtn) {
     btn.addEventListener("click", function(event){
-console.log(event)
+const name = event.target.parentNode.childNodes[1].innerText;
+const price = event.target.parentNode.childNodes[3].childNodes[1].innerText;
+const category = event.target.parentNode.childNodes[5].childNodes[1].innerText;
+
+console.log(event.target.parentNode.childNodes[5].childNodes[1].innerText)
     })
 }
 
