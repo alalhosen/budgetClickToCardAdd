f68cc29efa7c846ff1a72a94ba0de530a0b94c32
Alal Hosen <alalhosen1992620120@gmail.com>
2025-07-16 15:00:55 +0600

    add classList

--- app.js
+++ app.js
@@ -21,15 +21,17 @@ const price = event.target.parentNode.childNodes[3].childNodes[1].innerText;
 const category = event.target.parentNode.childNodes[5].childNodes[1].innerText;
 
 const selectedContainer = document.getElementById("selected-players-container");
+
 const div = document.createElement("div");
+div.classList.add("selected-player");
 
 const p1 = document.createElement("p");
 const p2 = document.createElement("p");
 const p3 = document.createElement("p");
-
 p1.innerText = name;
 p2.innerText = price;
 p3.innerText = category;
+
 div.appendChild(p1);
 div.appendChild(p2);
 div.appendChild(p3);
